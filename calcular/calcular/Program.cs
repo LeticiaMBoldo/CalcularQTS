acdb44faecc11af50bd4286182b68a9b842df7fa
LeticiaMBoldo <60079653+LeticiaMBoldo@users.noreply.github.com>
2020-02-13 20:27:51 -0300

    Subindo versão inicial
    aula de Qualidade e teste de software

---
+++ calcular/calcular/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace calcular
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calcular C = new Calcular();
+
+            Console.WriteLine("Informe o Valor inicial: ");
+            C.ValorInicial = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade das parcelas: ");
+            C.qtdeParcelas = int.Parse(Console.ReadLine());
+
+            double resultado = C.CalcularParcelas();
+
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Calcular
+    {
+        public double ValorInicial, resultado;
+        public int qtdeParcelas;
+
+        public double CalcularParcelas()
+        {
+            double parcelas = 0;
+
+            parcelas = qtdeParcelas * ValorInicial;
+
+            if (qtdeParcelas > 5)
+            {
+                parcelas = parcelas * (1 / 100);
+            }
+            return parcelas;
+        }
+
+
+    }
+}
